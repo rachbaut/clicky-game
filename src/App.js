f27108f16b4ee9bf5455fd8d579a7d5c59cbcfeb
Rachel Bautista <rachelbaut5@gmail.com>
2019-04-05 20:17:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import characters from './naruto.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: characters, 
+      clicks: 0,
+      highestClicks: 0
+    }
+  }
+
+  componentDidMount() {
+    this.shuffle()
+  }
+
+  shuffle = () => {
+    let newChar = this.state.characters.sort(() => {
+       return 0.5 - Math.random()
+    })
+    this.setState({
+      characters: newChar
+    })
+  }
+
+  handleClick = (id) => {
+    // console.log(id);
+    this.state.characters.map(character => {
+      if(character.id === id && character.selected !== true) {
+        let newClicks = this.state.clicks +1;
+        character.selected = true;
+        this.setState(prevState => {
+         return {clicks: prevState.clicks +1}
+        }) 
+        if(newClicks > this.state.highestClicks) {
+          this.setState({
+            highestClicks: newClicks
+          })
+        }
+        this.shuffle();
+      } else if(character.id === id && character.selected === true) {
+        characters.map(character => (
+         character.selected = false
+         ))
+        this.setState({
+          clicks: 0
+        })
+        this.shuffle();
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <p>Current Score: {this.state.clicks}</p>
+      <p>High Score: {this.state.highestClicks}</p>
+       {this.state.characters.map(character => (
+         <div key={character.id} className="card">
+           <img src={`/assets/images/${character.src}`} alt={character.alt} onClick={()=>this.handleClick(character.id)}></img>
+         </div>
+       ))}
       </div>
     );
   }
